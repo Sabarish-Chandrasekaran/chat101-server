b4c6dcf73db09a7105f086b4a27f01febd39b9ae
Sabarish Chandrasekaran <sabarish27k6@gmail.com>
2022-07-24 18:55:26 +0530

    changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
